152d192efa9bf3f8f9bb9f5406d0bde9e9e3eb5d
Juan Iriart <jeiriart@gmail.com>
2023-01-20 16:53:04 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php //Silen is gold
---
+++ plugin-skeleton.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin name: Plugin Skeleton
+ * Version: 1
+ * Plugin description: This plugin is for educational purposes only.
+ * Author: Juan Iriart
+ * Author URI: https://matecito.ar
+ * Text Domain: ske
+ */
